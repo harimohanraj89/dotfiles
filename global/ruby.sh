011132c5352c59f46830a6f430673db6acaf55d6
harimohanraj89 <hari.mohanraj89@gmail.com>
2017-01-06 14:35:40 -0500

    Add `rn` for `rails new` with sane defaults

--- global/ruby.sh
+++ global/ruby.sh
@@ -3,3 +3,13 @@ if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 alias rr="ruby"
 alias rs="rails s"
 alias be="bundle exec"
+
+rn () {
+  if [ -z "$1" ]
+  then
+    echo -e "Name of application missing.\nUsage:\n\trn cool_app"
+    return 1
+  else
+    rails new "$1" -d postgresql && cd "$1"
+  fi
+}
