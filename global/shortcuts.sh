961ce5e6764befec83f3cb954ff2a6bf49c5aade
harimohanraj89 <hari.mohanraj89@gmail.com>
2017-02-07 15:50:16 -0500

    Add shortcut() function to add aliases

--- global/shortcuts.sh
+++ global/shortcuts.sh
@@ -5,3 +5,9 @@ alias serve="ruby -run -e httpd . -p 5000"
 function gh {
   open "https://github.com/generalassembly/$1/pulls"
 }
+
+function shortcut {
+  touch "$DOTFILES_DIR/local/shortcuts.sh"
+  echo "alias $1='$2'" >> "$DOTFILES_DIR/local/shortcuts.sh"
+  source "$DOTFILES_DIR/local/shortcuts.sh"
+}
