d91722215462b8406de4cd534c9c78dafa0118cd
harimohanraj89 <hari.mohanraj89@gmail.com>
2017-02-13 17:57:51 -0500

    Update github light header

--- config/.js/github.com.js
+++ config/.js/github.com.js
@@ -1,3 +1,35 @@
 $(function() {
-  $('.header').removeClass('header-dark');
+  // Bring back the light header, inspired by @whylom
+  var css ='\
+    .header {                                                     \
+      background-color: #f3f3f3;                                  \
+      border-bottom: 1px solid #e5e5e5;                           \
+    }                                                             \
+    .header .header-nav-link {                                    \
+      color: #555;                                                \
+    }                                                             \
+    .header-logo-invertocat .octicon-mark-github {                \
+      color: #000;                                                \
+    }                                                             \
+    .header .header-search-scope {                                \
+      background: #e8f0f8;                                        \
+      color: #336479;                                             \
+      border-right: none;                                         \
+    }                                                             \
+    .header .header-search-wrapper.focus .header-search-scope {   \
+      background: #e9f4fc;                                        \
+      color: #44758a;                                             \
+      border-right: none;                                         \
+    }                                                             \
+    .header .header-search-input {                                \
+      border: 1px solid #e5e5e5;                                  \
+      color: #555;                                                \
+    }                                                             \
+    .header .header-search-input:focus {                          \
+      border-color: #bbb;                                         \
+    }                                                             \
+  ';
+
+  var style = $('<style></style>').html(css);
+  $('head').append(style);
 });
