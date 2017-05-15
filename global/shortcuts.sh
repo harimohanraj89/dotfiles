6da37a9c60a8d98f6634da53f56589845ad49355
harimohanraj89 <hari.mohanraj89@gmail.com>
2017-05-15 10:22:51 -0400

    Shortcut method queries existing shortcuts based on arguments

--- global/shortcuts.sh
+++ global/shortcuts.sh
@@ -7,7 +7,13 @@ function gh {
 }
 
 function shortcut {
-  touch "$DOTFILES_DIR/local/shortcuts.sh"
-  echo "alias $1='$2'" >> "$DOTFILES_DIR/local/shortcuts.sh"
-  source "$DOTFILES_DIR/local/shortcuts.sh"
+  if [ -z "$1" ] ; then
+    cat "$DOTFILES_DIR/local/shortcuts.sh"
+  elif [ -z "$2" ] ; then
+    cat "$DOTFILES_DIR/local/shortcuts.sh" | grep "alias $1"
+  else
+    touch "$DOTFILES_DIR/local/shortcuts.sh"
+    echo "alias $1='$2'" >> "$DOTFILES_DIR/local/shortcuts.sh"
+    source "$DOTFILES_DIR/local/shortcuts.sh"
+  fi
 }
