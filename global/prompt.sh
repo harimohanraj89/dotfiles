d919cda6e356570e24e714b4f1b500ffd400b620
Hari Mohanraj <hari.mohanraj89@gmail.com>
2020-05-19 12:06:54 +0530

    zsh support

--- global/prompt.sh
+++ global/prompt.sh
@@ -1,6 +1,1 @@
-style_user="\[${RESET}${WHITE}\]"
-style_path="\[${RESET}${CYAN}\]"
-style_chars="\[${RESET}${WHITE}\]"
-
-PS1="${style_user}\u | ${style_path}\w\n" # harimohanraj | ~/Documents/important
-PS1+="${style_chars}\$ \[${RESET}\]"      # $
+PROMPT="%n | %/ \$ "
